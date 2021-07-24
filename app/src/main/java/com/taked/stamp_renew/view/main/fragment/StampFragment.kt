5d353c0b0954947625a1aa524f7072b90718fdd8
Daichi Takeya <taked1664@gmail.com>
2021-07-24 22:26:06 +0900

    [add] implement SharedPreferences

--- app/src/main/java/com/taked/stamp_renew/view/main/fragment/StampFragment.kt
+++ app/src/main/java/com/taked/stamp_renew/view/main/fragment/StampFragment.kt
@@ -1,21 +1,35 @@
 package com.taked.stamp_renew.view.main.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.viewModels
 import com.taked.stamp_renew.databinding.FragmentStampBinding
+import com.taked.stamp_renew.model.ActivityState
 import com.taked.stamp_renew.viewmodel.main.StampViewModel
 
 class StampFragment : Fragment() {
 
-    private val viewModel = StampViewModel()
+    lateinit var viewModel: StampViewModel
     private lateinit var binding: FragmentStampBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
+        val judgeArray = Array(6) { false }
+        val clearArray = Array(6) { false }
+        val dataStore =
+            requireActivity().getSharedPreferences("DataStore", AppCompatActivity.MODE_PRIVATE)
+        for (i in 0..5) {
+            judgeArray[i] = dataStore.getBoolean("judge$i", false)
+            clearArray[i] = dataStore.getBoolean("clear$i", false)
+        }
+        viewModel = StampViewModel(judgeArray, clearArray)
+
         binding = FragmentStampBinding.inflate(inflater, container, false).apply {
             viewmodel = viewModel
             lifecycleOwner = viewLifecycleOwner
@@ -26,9 +40,32 @@ class StampFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        viewModel.isButtonClicked.observe(viewLifecycleOwner, { isEnabled ->
-//            binding.button.isEnabled = isEnabled
-//        })
+        viewModel.apply {
+            judgeInfo.observe(viewLifecycleOwner, { _ ->
+                val dataStore =
+                    requireActivity().getSharedPreferences(
+                        "DataStore",
+                        AppCompatActivity.MODE_PRIVATE
+                    )
+                val editor = dataStore.edit()
+                for ((count, bool) in viewModel.judgeInfo.value!!.withIndex()) {
+                    editor.putBoolean("judge$count", bool)
+                }
+                editor.apply()
+            })
+            clearInfo.observe(viewLifecycleOwner, { _ ->
+                val dataStore =
+                    requireActivity().getSharedPreferences(
+                        "DataStore",
+                        AppCompatActivity.MODE_PRIVATE
+                    )
+                val editor = dataStore.edit()
+                for ((count, bool) in viewModel.clearInfo.value!!.withIndex()) {
+                    editor.putBoolean("clear$count", bool)
+                }
+                editor.apply()
+            })
+        }
 //
 //        binding.inputText.addTextChangedListener { text ->
 //            val isInvalid = text.isNullOrBlank() || text.length < 4
