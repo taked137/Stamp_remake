ddb4bbc5485d91906e2918c09e727b1361bfabbc
Daichi Takeya <taked1664@gmail.com>
2021-08-01 03:00:23 +0900

    [add] add ProgressDialog

--- app/src/main/java/com/taked/stamp_renew/view/main/fragment/StampFragment.kt
+++ app/src/main/java/com/taked/stamp_renew/view/main/fragment/StampFragment.kt
@@ -1,20 +1,29 @@
 package com.taked.stamp_renew.view.main.fragment
 
+import android.animation.ObjectAnimator
+import android.content.Context
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
+import android.widget.ProgressBar
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.edit
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.LiveData
 import com.squareup.moshi.Moshi
+import com.taked.stamp_renew.R
+import com.taked.stamp_renew.databinding.CustomDialogBinding
 import com.taked.stamp_renew.databinding.FragmentStampBinding
 import com.taked.stamp_renew.model.ActivityState
 import com.taked.stamp_renew.view.main.StateData
 import com.taked.stamp_renew.view.main.StateKeys
+import com.taked.stamp_renew.view.util.AlertUtil
 import com.taked.stamp_renew.viewmodel.main.StampViewModel
 
 class StampFragment : Fragment() {
@@ -53,18 +62,18 @@ class StampFragment : Fragment() {
             lifecycleOwner = viewLifecycleOwner
         }
 
+        observeState(viewModel.judgeInfo, StateKeys.POSITION, container)
+        observeState(viewModel.clearInfo, StateKeys.QUIZ, container)
+
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        observeState(viewModel.judgeInfo, StateKeys.POSITION)
-        observeState(viewModel.clearInfo, StateKeys.QUIZ)
-    }
-
-    private fun observeState(array: LiveData<List<Boolean>>, stateKey: StateKeys) {
-        array.observe(viewLifecycleOwner, { _ ->
+    private fun observeState(
+        array: LiveData<List<Boolean>>, stateKey: StateKeys, container: ViewGroup?
+    ) {
+        array.observe(viewLifecycleOwner, {
+            AlertUtil.showProgressDialog(requireContext(), "ビーコン取得中...", container)
+            
             val clearArray = MutableList(6) { false }
             for ((count, bool) in array.value!!.withIndex()) {
                 clearArray[count] = bool
