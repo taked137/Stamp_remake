1347531b9db7fd60c7d5f8af3eb7d38e82d6481f
Daichi Takeya <taked1664@gmail.com>
2021-08-16 13:15:23 +0900

    [add] add APITest

--- app/src/test/java/com/taked/stamp_renew/APIUnitTest.kt
+++ app/src/test/java/com/taked/stamp_renew/APIUnitTest.kt
@@ -18,4 +18,48 @@ class APIUnitTest {
 
         Assert.assertNotEquals(response!!.uuid, "")
     }
+
+    @Test
+    fun testBeacon() {
+        val uuid = "uuid"
+        val quiz = 1
+        val beacon = arrayOf(1, 2, 3)
+
+        val response = runBlocking {
+            APIController.judgeBeacon(uuid, quiz, beacon.toMutableList())
+        }
+
+        Assert.assertTrue(response!!.url.contains("http"))
+    }
+
+    @Test
+    fun testQuizImage() {
+        val uuid = "uuid"
+        val quiz = 1
+
+        val response = runBlocking {
+            APIController.requestQuiz(uuid, quiz)
+        }
+
+        Assert.assertTrue(response!!.url.contains("http"))
+    }
+
+    @Test
+    fun testJudgeAnswer() {
+        val uuid = "uuid"
+        val quiz = 5
+        val answer = "はじっこ"
+
+        val correctResponse = runBlocking {
+            APIController.judgeAnswer(uuid, quiz, answer)
+        }
+
+        Assert.assertEquals(true, correctResponse!!.correct)
+
+        val wrongResponse = runBlocking {
+            APIController.judgeAnswer(uuid, quiz, quiz.toString())
+        }
+
+        Assert.assertEquals(false, wrongResponse!!.correct)
+    }
 }
